09e49f2a35f1a11b3145e0c9b75dee167634aec4
Vianney Veremme <10519369+Vianpyro@users.noreply.github.com>
2024-05-20 12:31:47 -0400

    Create create_tables.sql

---
+++ create_tables.sql
@@ -0,0 +1,47 @@
+DROP TABLE IF EXISTS recipe;
+DROP TABLE IF EXISTS recipe_tag;
+DROP TABLE IF EXISTS tag;
+
+
+CREATE TABLE recipe (
+    recipe_id INT AUTO_INCREMENT PRIMARY KEY,
+    author VARCHAR(255),
+    title VARCHAR(255),
+    description TEXT,
+    ingredients TEXT,
+    preparation TEXT,
+    publication_date DATETIME,
+    modification_date DATETIME,
+    likes INT,
+    image VARCHAR(255),
+    cuisine VARCHAR(255),
+    course VARCHAR(255),
+    prep_time INT,
+    cook_time INT,
+    total_time INT,
+    servings INT,
+    difficulty_level VARCHAR(255),
+    rating FLOAT,
+    number_of_reviews INT,
+    video_url VARCHAR(255),
+    nutritional_information TEXT,
+    comments TEXT,
+    source_url VARCHAR(255),
+    user_favorites INT,
+    allergen_information TEXT,
+    flavor_profile VARCHAR(255),
+    status ENUM('verified', 'liked', 'hidden', 'banned') NULL
+);
+
+CREATE TABLE tag (
+    tag_id INT AUTO_INCREMENT PRIMARY KEY,
+    tag_name VARCHAR(255) UNIQUE
+);
+
+CREATE TABLE recipe_tag (
+    recipe_id INT,
+    tag_id INT,
+    PRIMARY KEY (recipe_id, tag_id),
+    FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id) ON DELETE CASCADE,
+    FOREIGN KEY (tag_id) REFERENCES tag(tag_id) ON DELETE CASCADE
+);
