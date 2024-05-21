16f4b8d9443e40180c4d21c10f6ef07fa2ece438
Vianney Veremme <10519369+Vianpyro@users.noreply.github.com>
2024-05-21 19:03:36 -0400

    Added procedure to create new recipe

--- create_procedures.sql
+++ create_procedures.sql
@@ -18,4 +18,60 @@ BEGIN
     SET p_user_id = LAST_INSERT_ID();
 END$$
 
+CREATE PROCEDURE create_recipe(
+    IN p_author_id INT,
+    IN p_title VARCHAR(255),
+    IN p_description TEXT,
+    IN p_ingredients TEXT,
+    IN p_preparation TEXT,
+    IN p_image VARCHAR(255) NULL,
+    IN p_cook_time INT NULL,
+    IN p_difficulty_level VARCHAR(255) NULL,
+    IN p_nutritional_information TEXT NULL,
+    IN p_source VARCHAR(255) NULL,
+    IN p_allergen_information TEXT NULL,
+    IN p_video_url VARCHAR(255) NULL,
+    OUT p_recipe_id INT
+)
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        SET p_recipe_id = -1;
+    END;
+
+    INSERT INTO recipe (
+        author_id,
+        title,
+        description,
+        ingredients,
+        preparation,
+        publication_date,
+        image,
+        cook_time,
+        difficulty_level,
+        nutritional_information,
+        source,
+        allergen_information,
+        video_url,
+        status
+    ) VALUES (
+        p_author_id,
+        p_title,
+        p_description,
+        p_ingredients,
+        p_preparation,
+        NOW(),
+        COALESCE(p_image, NULL),
+        COALESCE(p_cook_time, NULL),
+        COALESCE(p_difficulty_level, NULL),
+        COALESCE(p_nutritional_information, NULL),
+        COALESCE(p_source, NULL),
+        COALESCE(p_allergen_information, NULL),
+        COALESCE(p_video_url, NULL),
+        COALESCE(status, NULL)
+    );
+
+    SET p_recipe_id = LAST_INSERT_ID();
+END$$
+
 DELIMITER ;
