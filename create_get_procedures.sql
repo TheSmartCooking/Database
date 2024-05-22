1ae273332acca5308d4522b173de0289766c6e23
Vianney Veremme <10519369+Vianpyro@users.noreply.github.com>
2024-05-22 08:15:54 -0400

    Added GetRecipeById

--- create_get_procedures.sql
+++ create_get_procedures.sql
@@ -9,9 +9,36 @@ BEGIN
         r.difficulty_level,
         r.status,
         r.rating,
-        i.image_path
+        i.image_path AS image
     FROM recipe r
     LEFT JOIN image i ON r.image_id = i.image_id;
+END$$
+
+CREATE PROCEDURE GetRecipeById(IN recipeId INT)
+BEGIN
+    SELECT 
+        r.recipe_id,
+        r.title,
+        r.description,
+        r.ingredients,
+        r.preparation,
+        r.publication_date,
+        r.modification_date,
+        r.cook_time,
+        r.difficulty_level,
+        r.rating,
+        r.number_of_reviews,
+        r.nutritional_information,
+        r.source,
+        r.allergen_information,
+        r.video_url,
+        r.status,
+        u.username AS author,
+        i.image_path AS image
+    FROM recipe r
+    LEFT JOIN user u ON r.author_id = u.user_id
+    LEFT JOIN image i ON r.image_id = i.image_id
+    WHERE r.recipe_id = recipeId;
 END$$
 
 DELIMITER ;
