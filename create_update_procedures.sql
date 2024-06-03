f7ac394b37cfd792ccf1fb7b53d0e4e76db8a86d
Vianpyro <vianney@veremme.org>
2024-06-02 21:27:03 -0400

    Created procedure to update last login date

---
+++ create_update_procedures.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+
+CREATE PROCEDURE update_last_login(
+    IN p_person_id INT
+)
+BEGIN
+    UPDATE person
+    SET last_login = CURRENT_TIMESTAMP
+    WHERE person_id = p_person_id;
+END$$
+
+DELIMITER ;
