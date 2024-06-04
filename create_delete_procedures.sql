1659c9e254d005a4defde684285e2c8e906162a4
Vianpyro <vianney@veremme.org>
2024-06-04 17:34:06 -0400

    Added delete procedures

---
+++ create_delete_procedures.sql
@@ -0,0 +1,31 @@
+DROP PROCEDURE IF EXISTS delete_favorite;
+
+DELIMITER $$
+
+CREATE PROCEDURE delete_favorite(
+    IN p_person_id INT,
+    IN p_recipe_id INT
+)
+BEGIN
+    DELETE FROM favorite
+    WHERE person_id = p_person_id AND recipe_id = p_recipe_id;
+END$$
+
+CREATE PROCEDURE delete_comment(
+    IN p_comment_id INT
+)
+BEGIN
+    DELETE FROM comment
+    WHERE comment_id = p_comment_id;
+END$$
+
+CREATE PROCEDURE delete_comment_like(
+    IN p_person_id INT,
+    IN p_comment_id INT
+)
+BEGIN
+    DELETE FROM comment_like
+    WHERE person_id = p_person_id AND comment_id = p_comment_id;
+END$$
+
+DELIMITER ;
