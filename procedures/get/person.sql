db3a0ce462c2c91e58ab648bee4f1beb621641f0
Vianpyro <vianney@veremme.org>
2024-12-01 22:22:10 -0500

    Refactor person retrieval procedures to exclude email and last_login for improved data handling

--- procedures/get/person.sql
+++ procedures/get/person.sql
@@ -5,20 +5,20 @@ DELIMITER //
 
 CREATE OR REPLACE PROCEDURE get_all_persons()
 BEGIN
-    SELECT person_id, person_name, email, registration_date, last_login, locale_id
+    SELECT person_id, person_name, locale_id
     FROM person;
 END //
 
 CREATE OR REPLACE PROCEDURE get_person_by_id(IN p_person_id INT)
 BEGIN
-    SELECT person_id, person_name, email, registration_date, last_login, locale_id
+    SELECT person_id, person_name, locale_id
     FROM person
     WHERE person_id = p_person_id;
 END //
 
 CREATE OR REPLACE PROCEDURE get_person_by_email(IN p_email VARCHAR(100))
 BEGIN
-    SELECT person_id, person_name, email, registration_date, last_login, locale_id
+    SELECT person_id, person_name, locale_id
     FROM person
     WHERE email = p_email;
 END //
@@ -40,14 +40,14 @@ END //
 
 CREATE OR REPLACE PROCEDURE get_recent_persons(IN p_start_date TIMESTAMP, IN p_end_date TIMESTAMP)
 BEGIN
-    SELECT person_id, person_name, email, registration_date
+    SELECT person_id, person_name, registration_date
     FROM person
     WHERE registration_date BETWEEN p_start_date AND p_end_date;
 END //
 
 CREATE OR REPLACE PROCEDURE get_persons_by_locale(IN p_locale_id INT)
 BEGIN
-    SELECT person_id, person_name, email, registration_date
+    SELECT person_id, person_name
     FROM person
     WHERE locale_id = p_locale_id;
 END //
