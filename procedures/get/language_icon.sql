68958e2daae722bde04abe441ef1ea62d937b08b
Vianpyro <vianney@veremme.org>
2024-12-01 22:27:21 -0500

    Add stored procedures for retrieving language icons

---
+++ procedures/get/language_icon.sql
@@ -0,0 +1,17 @@
+-- Use the database
+USE smartcooking;
+
+DELIMITER //
+
+CREATE PROCEDURE get_all_language_icons()
+BEGIN
+    SELECT * FROM language_icon;
+END //
+
+CREATE PROCEDURE get_language_icon(IN p_language_id INT)
+BEGIN
+    SELECT language_icon_id FROM language_icon
+    WHERE language_id = p_language_id;
+END //
+
+DELIMITER ;
