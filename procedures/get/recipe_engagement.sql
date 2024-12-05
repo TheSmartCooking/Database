58614060760f13069f52c161df76912c46c0ac31
Vianpyro <vianney@veremme.org>
2024-12-04 22:50:14 -0500

    Simplify recipe engagement procedures to select all columns

--- procedures/get/recipe_engagement.sql
+++ procedures/get/recipe_engagement.sql
@@ -3,8 +3,7 @@ DELIMITER //
 -- This procedure is intended for testing purposes only
 CREATE PROCEDURE get_all_recipe_engagements ()
 BEGIN
-    SELECT engagement_id, person_id, recipe_id, engagement_type, engagement_date
-    FROM recipe_engagement;
+    SELECT * FROM recipe_engagement;
 END //
 
 CREATE PROCEDURE get_engagement_counts_by_recipe (
@@ -21,8 +20,7 @@ CREATE PROCEDURE get_all_engagements_by_person (
     IN p_person_id INT
 )
 BEGIN
-    SELECT engagement_id, person_id, recipe_id, engagement_type, engagement_date
-    FROM recipe_engagement
+    SELECT * FROM recipe_engagement
     WHERE person_id = p_person_id;
 END //
 
@@ -30,8 +28,7 @@ CREATE PROCEDURE get_all_engagements_by_type (
     IN p_engagement_type ENUM('like', 'favorite', 'view')
 )
 BEGIN
-    SELECT engagement_id, person_id, recipe_id, engagement_type, engagement_date
-    FROM recipe_engagement
+    SELECT * FROM recipe_engagement
     WHERE engagement_type = p_engagement_type;
 END //
 
