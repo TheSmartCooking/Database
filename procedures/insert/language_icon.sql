940310118cb9f663e006f67fe88e06ece4a8964c
Vianpyro <vianney@veremme.org>
2024-12-06 20:37:55 -0500

    Add procedure to insert language icon by ISO code

--- procedures/insert/language_icon.sql
+++ procedures/insert/language_icon.sql
@@ -12,4 +12,17 @@ BEGIN
     VALUES (p_language_id, p_icon_picture_id);
 END //
 
+CREATE OR REPLACE PROCEDURE insert_language_icon_by_iso_code(
+    IN p_iso_code CHAR(2),
+    IN p_icon_picture_id INT
+)
+BEGIN
+    DECLARE v_language_id INT;
+
+    SELECT language_id INTO v_language_id
+    FROM lang
+    WHERE iso_code = p_iso_code;
+
+    CALL insert_language_icon(v_language_id, p_icon_picture_id);
+END //
 DELIMITER ;
