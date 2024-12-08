88b1e31c45a2e2d8d6a2bb27fe75aa2a893147a3
Vianpyro <vianney@veremme.org>
2024-12-07 22:09:22 -0500

    Add procedure to retrieve ingredient by ID

--- procedures/get/ingredient.sql
+++ procedures/get/ingredient.sql
@@ -9,4 +9,10 @@ BEGIN
     SELECT * FROM ingredient;
 END //
 
+CREATE OR REPLACE PROCEDURE get_ingredient_by_id(IN p_ingredient_id INT)
+BEGIN
+    SELECT default_name FROM ingredient
+    WHERE ingredient_id = p_ingredient_id;
+END //
+
 DELIMITER ;
