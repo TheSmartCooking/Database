7cff59e4d37e8b777f814018c111bbca1b317ef2
Vianpyro <vianney@veremme.org>
2024-12-08 12:49:10 -0500

    Remove unused procedures for retrieving recent persons and persons by locale to streamline codebase

--- procedures/get/person.sql
+++ procedures/get/person.sql
@@ -39,25 +39,4 @@ BEGIN
     WHERE p.person_id = p_person_id;
 END //
 
-CREATE OR REPLACE PROCEDURE get_recent_persons(IN p_start_date TIMESTAMP, IN p_end_date TIMESTAMP)
-BEGIN
-    SELECT person_id, person_name, registration_date
-    FROM person
-    WHERE registration_date BETWEEN p_start_date AND p_end_date;
-END //
-
-CREATE OR REPLACE PROCEDURE get_persons_by_locale(IN p_language_id INT)
-BEGIN
-    SELECT person_id, person_name
-    FROM person
-    WHERE language_id = p_language_id;
-END //
-
-CREATE OR REPLACE PROCEDURE get_person_recipe_engagement(IN p_person_id INT)
-BEGIN
-    SELECT re.recipe_id, re.engagement_type, re.engagement_date
-    FROM recipe_engagement re
-    WHERE re.person_id = p_person_id;
-END //
-
 DELIMITER ;
