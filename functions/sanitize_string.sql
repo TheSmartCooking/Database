08484f60c0cc910607e9f744f8ec2fbffba1d990
Vianpyro <vianney@veremme.org>
2024-12-24 15:13:07 -0500

    Enhance sanitize_string function to escape additional SQL special characters

--- functions/sanitize_string.sql
+++ functions/sanitize_string.sql
@@ -9,9 +9,13 @@ DETERMINISTIC
 BEGIN
   DECLARE safe_keyword VARCHAR(255);
 
-  -- Escape special SQL wildcard characters '%' and '_'
-  SET safe_keyword = REPLACE(p_keyword, '%', '\\%');
-  SET safe_keyword = REPLACE(safe_keyword, '_', '\\_');
+  -- Escape SQL special characters
+  SET safe_keyword = REPLACE(p_keyword, '\\', '\\\\'); -- Escape backslash
+  SET safe_keyword = REPLACE(safe_keyword, '%', '\\%'); -- Escape percent
+  SET safe_keyword = REPLACE(safe_keyword, '_', '\\_'); -- Escape underscore
+  SET safe_keyword = REPLACE(safe_keyword, '\'', '\\\''); -- Escape single quote
+  SET safe_keyword = REPLACE(safe_keyword, '"', '\\"'); -- Escape double quote
+  SET safe_keyword = REPLACE(safe_keyword, ';', '\\;'); -- Escape semicolon
 
   -- Return the safe keyword with wildcards added
   RETURN CONCAT('%', safe_keyword, '%');
