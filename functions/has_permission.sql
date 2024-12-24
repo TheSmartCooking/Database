fa3d8f81ca4431cbf0f66456e2ca74327781af67
Vianpyro <vianney@veremme.org>
2024-12-24 16:52:04 -0500

    Refactor has_permission function to use responsibility_name instead of responsibility_id

--- functions/has_permission.sql
+++ functions/has_permission.sql
@@ -3,16 +3,18 @@ USE smartcooking;
 
 DELIMITER //
 
-CREATE OR REPLACE FUNCTION has_permission(p_person_id INT, p_responsibility_id INT)
+CREATE OR REPLACE FUNCTION has_permission(p_person_id INT, p_responsibility_name VARCHAR(100))
 RETURNS BOOLEAN
 DETERMINISTIC
 BEGIN
-    DECLARE result INT;
+    DECLARE v_result INT;
+
     SELECT COUNT(*)
-    INTO result
-    FROM person_responsibility
-    WHERE person_id = p_person_id AND responsibility_id = p_responsibility_id;
-    RETURN result > 0;
+    INTO v_result
+    FROM person_responsibility pr
+    INNER JOIN responsibility r ON pr.responsibility_id = r.responsibility_id
+    WHERE pr.person_id = p_person_id AND r.responsibility_name = p_responsibility_name;
+    RETURN v_result > 0;
 END //
 
 DELIMITER ;
