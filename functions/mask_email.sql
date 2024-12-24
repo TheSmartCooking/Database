1a60e560711257d9db51f8775b4f0f8395c4838f
Vianpyro <vianney@veremme.org>
2024-12-24 12:10:23 -0500

    Enhance email masking function to handle short local parts correctly

--- functions/mask_email.sql
+++ functions/mask_email.sql
@@ -7,13 +7,23 @@ CREATE OR REPLACE FUNCTION mask_email(p_email VARCHAR(100))
 RETURNS VARCHAR(100)
 DETERMINISTIC
 BEGIN
-    RETURN CONCAT(
-        LEFT(p_email, 1),
-        REPEAT('*', LENGTH(SUBSTRING_INDEX(p_email, '@', 1)) - 2),
-        RIGHT(SUBSTRING_INDEX(p_email, '@', 1), 1),
-        '@',
-        SUBSTRING_INDEX(p_email, '@', -1)
-    );
+    DECLARE local_part VARCHAR(100);
+    DECLARE domain_part VARCHAR(100);
+
+    SET local_part = SUBSTRING_INDEX(p_email, '@', 1);
+    SET domain_part = SUBSTRING_INDEX(p_email, '@', -1);
+
+    IF LENGTH(local_part) <= 2 THEN
+        RETURN CONCAT(local_part, '@', domain_part);
+    ELSE
+        RETURN CONCAT(
+            LEFT(local_part, 1),
+            REPEAT('*', LENGTH(local_part) - 2),
+            RIGHT(local_part, 1),
+            '@',
+            domain_part
+        );
+    END IF;
 END //
 
 DELIMITER ;
