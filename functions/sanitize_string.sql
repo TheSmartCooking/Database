bb83b9412fe7193a55dc4a88cc054c9c830bec51
Vianpyro <vianney@veremme.org>
2024-12-24 15:48:26 -0500

    Refactor variable names in SQL functions and procedures for consistency and clarity

--- functions/sanitize_string.sql
+++ functions/sanitize_string.sql
@@ -7,18 +7,18 @@ CREATE FUNCTION sanitize_string(p_string VARCHAR(255))
 RETURNS VARCHAR(255)
 DETERMINISTIC
 BEGIN
-  DECLARE safe_string VARCHAR(255);
+  DECLARE v_safe_string VARCHAR(255);
 
   -- Escape SQL special characters
-  SET safe_string = REPLACE(p_string, '\\', '\\\\'); -- Escape backslash
-  SET safe_string = REPLACE(safe_string, '%', '\\%'); -- Escape percent
-  SET safe_string = REPLACE(safe_string, '_', '\\_'); -- Escape underscore
-  SET safe_string = REPLACE(safe_string, '\'', '\\\''); -- Escape single quote
-  SET safe_string = REPLACE(safe_string, '"', '\\"'); -- Escape double quote
-  SET safe_string = REPLACE(safe_string, ';', '\\;'); -- Escape semicolon
+  SET v_safe_string = REPLACE(p_string, '\\', '\\\\'); -- Escape backslash
+  SET v_safe_string = REPLACE(v_safe_string, '%', '\\%'); -- Escape percent
+  SET v_safe_string = REPLACE(v_safe_string, '_', '\\_'); -- Escape underscore
+  SET v_safe_string = REPLACE(v_safe_string, '\'', '\\\''); -- Escape single quote
+  SET v_safe_string = REPLACE(v_safe_string, '"', '\\"'); -- Escape double quote
+  SET v_safe_string = REPLACE(v_safe_string, ';', '\\;'); -- Escape semicolon
 
   -- Return the safe string with wildcards added
-  RETURN CONCAT('%', safe_string, '%');
+  RETURN CONCAT('%', v_safe_string, '%');
 END //
 
 DELIMITER ;
