08945b9fc12410d050b886bb64717b3699bbfba1
Vianpyro <vianney@veremme.org>
2024-12-28 17:48:11 -0500

    Add pagination functions and refactor recipe procedures for improved efficiency

--- procedures/get/recipe.sql
+++ procedures/get/recipe.sql
@@ -3,12 +3,13 @@ USE smartcooking;
 
 DELIMITER //
 
--- This procedure is intended for testing purposes only
+-- Testing procedure (unchanged as it does not benefit from the functions)
 CREATE OR REPLACE PROCEDURE get_all_recipes()
 BEGIN
     SELECT * FROM recipe;
 END //
 
+-- Procedure to fetch recipe details by ID, using the `get_recipe_title` function
 CREATE OR REPLACE PROCEDURE get_recipe_by_id(
     IN p_recipe_id INT,
     IN p_language_iso_code CHAR(2)
@@ -22,7 +23,7 @@ BEGIN
         r.difficulty_level,
         r.number_of_reviews,
         r.recipe_status,
-        rt.title,
+        get_recipe_title(p_recipe_id, p_language_iso_code) AS title,
         rt.details,
         rt.preparation,
         rt.nutritional_information,
@@ -35,17 +36,18 @@ BEGIN
       AND l.iso_code = p_language_iso_code;
 END //
 
+-- Generic template for pagination with limit and offset
 CREATE OR REPLACE PROCEDURE get_all_recipes_paginated(
     IN p_limit INT,
     IN p_offset INT,
     IN p_language_iso_code CHAR(2)
 )
 BEGIN
-    DECLARE v_max_limit INT DEFAULT 30;
     DECLARE v_offset INT;
+    DECLARE v_limit INT;
 
-    SET v_offset = (p_offset - 1) * p_limit;
-    SET p_limit = LEAST(p_limit, v_max_limit);
+    SET v_offset = calculate_offset(p_offset, p_limit);
+    SET v_limit = enforce_row_limit(p_limit);
 
     SELECT
         r.recipe_id,
@@ -56,13 +58,13 @@ BEGIN
         r.difficulty_level,
         r.number_of_reviews,
         r.recipe_status,
-        rt.title
+        get_recipe_title(r.recipe_id, p_language_iso_code) AS title
     FROM recipe r
     INNER JOIN recipe_translation rt ON r.recipe_id = rt.recipe_id
     INNER JOIN lang l ON rt.language_id = l.language_id
     INNER JOIN person p ON r.author_id = p.person_id
     WHERE l.iso_code = p_language_iso_code
-    LIMIT p_limit OFFSET v_offset;
+    LIMIT v_limit OFFSET v_offset;
 END //
 
 CREATE OR REPLACE PROCEDURE get_recipes_by_author_paginated(
@@ -72,11 +74,11 @@ CREATE OR REPLACE PROCEDURE get_recipes_by_author_paginated(
     IN p_language_iso_code CHAR(2)
 )
 BEGIN
-    DECLARE v_max_limit INT DEFAULT 30;
     DECLARE v_offset INT;
+    DECLARE v_limit INT;
 
-    SET v_offset = (p_offset - 1) * p_limit;
-    SET p_limit = LEAST(p_limit, v_max_limit);
+    SET v_offset = calculate_offset(p_offset, p_limit);
+    SET v_limit = enforce_row_limit(p_limit);
 
     SELECT
         r.recipe_id,
@@ -85,13 +87,13 @@ BEGIN
         r.difficulty_level,
         r.number_of_reviews,
         r.recipe_status,
-        rt.title
+        get_recipe_title(r.recipe_id, p_language_iso_code) AS title
     FROM recipe r
     INNER JOIN recipe_translation rt ON r.recipe_id = rt.recipe_id
     INNER JOIN lang l ON rt.language_id = l.language_id
     WHERE r.author_id = p_author_id
       AND l.iso_code = p_language_iso_code
-    LIMIT p_limit OFFSET v_offset;
+    LIMIT v_limit OFFSET v_offset;
 END //
 
 CREATE OR REPLACE PROCEDURE get_top_rated_recipes_paginated(
@@ -101,11 +103,11 @@ CREATE OR REPLACE PROCEDURE get_top_rated_recipes_paginated(
     IN p_language_iso_code CHAR(2)
 )
 BEGIN
-    DECLARE v_max_limit INT DEFAULT 30;
     DECLARE v_offset INT;
+    DECLARE v_limit INT;
 
-    SET v_offset = (p_offset - 1) * p_limit;
-    SET p_limit = LEAST(p_limit, v_max_limit);
+    SET v_offset = calculate_offset(p_offset, p_limit);
+    SET v_limit = enforce_row_limit(p_limit);
 
     SELECT
         r.recipe_id,
@@ -117,7 +119,7 @@ BEGIN
         r.number_of_reviews,
         r.recipe_status,
         AVG(rr.rating) AS average_rating,
-        rt.title
+        get_recipe_title(r.recipe_id, p_language_iso_code) AS title
     FROM recipe r
     INNER JOIN recipe_rating rr ON r.recipe_id = rr.recipe_id
     INNER JOIN recipe_translation rt ON r.recipe_id = rt.recipe_id
@@ -127,7 +129,7 @@ BEGIN
     GROUP BY r.recipe_id
     HAVING average_rating >= p_min_rating
     ORDER BY average_rating DESC
-    LIMIT p_limit OFFSET v_offset;
+    LIMIT v_limit OFFSET v_offset;
 END //
 
 CREATE OR REPLACE PROCEDURE get_recipes_liked_by_person_paginated(
@@ -136,11 +138,11 @@ CREATE OR REPLACE PROCEDURE get_recipes_liked_by_person_paginated(
     IN p_offset INT
 )
 BEGIN
-    DECLARE v_max_limit INT DEFAULT 30;
     DECLARE v_offset INT;
+    DECLARE v_limit INT;
 
-    SET v_offset = (p_offset - 1) * p_limit;
-    SET p_limit = LEAST(p_limit, v_max_limit);
+    SET v_offset = calculate_offset(p_offset, p_limit);
+    SET v_limit = enforce_row_limit(p_limit);
 
     SELECT
         r.recipe_id,
@@ -156,7 +158,7 @@ BEGIN
     INNER JOIN person p ON r.author_id = p.person_id
     WHERE re.person_id = p_person_id
       AND re.engagement_type = 'like'
-    LIMIT p_limit OFFSET v_offset;
+    LIMIT v_limit OFFSET v_offset;
 END //
 
 CREATE OR REPLACE PROCEDURE get_recipes_by_category_paginated(
@@ -166,11 +168,11 @@ CREATE OR REPLACE PROCEDURE get_recipes_by_category_paginated(
     IN p_language_iso_code CHAR(2)
 )
 BEGIN
-    DECLARE v_max_limit INT DEFAULT 30;
     DECLARE v_offset INT;
+    DECLARE v_limit INT;
 
-    SET v_offset = (p_offset - 1) * p_limit;
-    SET p_limit = LEAST(p_limit, v_max_limit);
+    SET v_offset = calculate_offset(p_offset, p_limit);
+    SET v_limit = enforce_row_limit(p_limit);
 
     SELECT
         r.recipe_id,
@@ -181,7 +183,7 @@ BEGIN
         r.difficulty_level,
         r.number_of_reviews,
         r.recipe_status,
-        rt.title
+        get_recipe_title(r.recipe_id, p_language_iso_code) AS title
     FROM recipe r
     INNER JOIN recipe_category rc ON r.recipe_id = rc.recipe_id
     INNER JOIN recipe_translation rt ON r.recipe_id = rt.recipe_id
@@ -189,7 +191,7 @@ BEGIN
     INNER JOIN person p ON r.author_id = p.person_id
     WHERE rc.category_id = p_category_id
       AND l.iso_code = p_language_iso_code
-    LIMIT p_limit OFFSET v_offset;
+    LIMIT v_limit OFFSET v_offset;
 END //
 
 CREATE OR REPLACE PROCEDURE get_recipes_by_tags_paginated(
@@ -199,11 +201,11 @@ CREATE OR REPLACE PROCEDURE get_recipes_by_tags_paginated(
     IN p_language_iso_code CHAR(2)
 )
 BEGIN
-    DECLARE v_max_limit INT DEFAULT 30;
     DECLARE v_offset INT;
+    DECLARE v_limit INT;
 
-    SET v_offset = (p_offset - 1) * p_limit;
-    SET p_limit = LEAST(p_limit, v_max_limit);
+    SET v_offset = calculate_offset(p_offset, p_limit);
+    SET v_limit = enforce_row_limit(p_limit);
 
     SELECT
         r.recipe_id,
@@ -214,7 +216,7 @@ BEGIN
         r.difficulty_level,
         r.number_of_reviews,
         r.recipe_status,
-        rt.title
+        get_recipe_title(r.recipe_id, p_language_iso_code) AS title
     FROM recipe r
     INNER JOIN recipe_tag rtg ON r.recipe_id = rtg.recipe_id
     INNER JOIN recipe_translation rt ON r.recipe_id = rt.recipe_id
@@ -222,7 +224,7 @@ BEGIN
     INNER JOIN person p ON r.author_id = p.person_id
     WHERE JSON_CONTAINS(p_tags, JSON_QUOTE(rtg.tag))
       AND l.iso_code = p_language_iso_code
-    LIMIT p_limit OFFSET v_offset;
+    LIMIT v_limit OFFSET v_offset;
 END //
 
 CREATE OR REPLACE PROCEDURE get_recipes_by_name_paginated(
@@ -232,11 +234,13 @@ CREATE OR REPLACE PROCEDURE get_recipes_by_name_paginated(
     IN p_language_iso_code CHAR(2)
 )
 BEGIN
-    DECLARE v_max_limit INT DEFAULT 30;
     DECLARE v_offset INT;
+    DECLARE v_limit INT;
+    DECLARE v_safe_recipe_name VARCHAR(255);
 
-    SET v_offset = (p_offset - 1) * p_limit;
-    SET p_limit = LEAST(p_limit, v_max_limit);
+    SET v_offset = calculate_offset(p_offset, p_limit);
+    SET v_limit = enforce_row_limit(p_limit);
+    SET v_safe_recipe_name = sanitize_string(p_name);
 
     SELECT
         r.recipe_id,
@@ -247,14 +251,14 @@ BEGIN
         r.difficulty_level,
         r.number_of_reviews,
         r.recipe_status,
-        rt.title
+        get_recipe_title(r.recipe_id, p_language_iso_code) AS title
     FROM recipe r
     INNER JOIN recipe_translation rt ON r.recipe_id = rt.recipe_id
     INNER JOIN lang l ON rt.language_id = l.language_id
     INNER JOIN person p ON r.author_id = p.person_id
     WHERE rt.title LIKE v_safe_recipe_name ESCAPE '\\'
       AND l.iso_code = p_language_iso_code
-    LIMIT p_limit OFFSET v_offset;
+    LIMIT v_limit OFFSET v_offset;
 END //
 
 DELIMITER ;
