b7e3e770f5f112dd28a3931ea528e040ea5900ce
Vianpyro <vianney@veremme.org>
2024-12-28 23:24:32 -0500

    Refactor SQL calls for recipe ingredients and translations to improve readability by consolidating statements; update SQLFluff configuration to ignore max line length

--- setup/z_mock/recipe_ingredient.sql
+++ setup/z_mock/recipe_ingredient.sql
@@ -4,9 +4,5 @@ USE smartcooking;
 -- Fill the database with mock data
 CALL insert_recipe_ingredient(1, 1, 2, 'g');
 CALL insert_recipe_ingredient_by_name(1, 'Sugar', 1, 'tsp');
-CALL insert_recipe_ingredients(
-    2, '[{"ingredient_id": 1, "quantity": 2, "unit": "g"}, {"ingredient_id": 2, "quantity": 1, "unit": "tsp"}]'
-);
-CALL insert_recipe_ingredients_by_name(
-    3, '[{"ingredient_name": "Salt", "quantity": 2, "unit": "g"}, {"ingredient_name": "Pepper", "quantity": 1, "unit": "tsp"}]'
-);
+CALL insert_recipe_ingredients(2, '[{"ingredient_id": 1, "quantity": 2, "unit": "g"}, {"ingredient_id": 2, "quantity": 1, "unit": "tsp"}]');
+CALL insert_recipe_ingredients_by_name(3, '[{"ingredient_name": "Salt", "quantity": 2, "unit": "g"}, {"ingredient_name": "Pepper", "quantity": 1, "unit": "tsp"}]');
--- setup/z_mock/recipe_translation.sql
+++ setup/z_mock/recipe_translation.sql
@@ -2,182 +2,23 @@
 USE smartcooking;
 
 -- Fill the database with mock data
-CALL insert_recipe_translation_by_iso_code(
-    1, 'en',
-    'Title 1',
-    'Details 1',
-    'Preparation 1',
-    'Nutritional information 1',
-    'www.video.com'
-);
-
-CALL insert_recipe_translation_by_iso_code(
-    1, 'es',
-    'Título 1',
-    'Detalles 1',
-    'Preparación 1',
-    'Información nutricional 1',
-    'www.video_es.com'
-);
-
-CALL insert_recipe_translation_by_iso_code(
-    1, 'fr',
-    'Titre 1',
-    'Détails 1',
-    'Préparation 1',
-    'Informations nutritionnelles 1',
-    'www.video_fr.com'
-);
-
-CALL insert_recipe_translation_by_iso_code(
-    1, 'ko',
-    '제목 1',
-    '세부 정보 1',
-    '준비 1',
-    '영양 정보 1',
-    'www.video_ko.com'
-);
-
-CALL insert_recipe_translation_by_iso_code(
-    2, 'en',
-    'Title 2',
-    'Details 2',
-    'Preparation 2',
-    'Nutritional information 2',
-    'www.video.com'
-);
-
-CALL insert_recipe_translation_by_iso_code(
-    2, 'es',
-    'Título 2',
-    'Detalles 2',
-    'Preparación 2',
-    'Información nutricional 2',
-    'www.video_es.com'
-);
-
-CALL insert_recipe_translation_by_iso_code(
-    2, 'fr',
-    'Titre 2',
-    'Détails 2',
-    'Préparation 2',
-    'Informations nutritionnelles 2',
-    'www.video_fr.com'
-);
-
-CALL insert_recipe_translation_by_iso_code(
-    2, 'ko',
-    '제목 2',
-    '세부 정보 2',
-    '준비 2',
-    '영양 정보 2',
-    'www.video_ko.com'
-);
-
-CALL insert_recipe_translation_by_iso_code(
-    3, 'en',
-    'Title 3',
-    'Details 3',
-    'Preparation 3',
-    'Nutritional information 3',
-    'www.video.com'
-);
-
-CALL insert_recipe_translation_by_iso_code(
-    3, 'es',
-    'Título 3',
-    'Detalles 3',
-    'Preparación 3',
-    'Información nutricional 3',
-    'www.video_es.com'
-);
-
-CALL insert_recipe_translation_by_iso_code(
-    3, 'fr',
-    'Titre 3',
-    'Détails 3',
-    'Préparation 3',
-    'Informations nutritionnelles 3',
-    'www.video_fr.com'
-);
-
-CALL insert_recipe_translation_by_iso_code(
-    3, 'ko',
-    '제목 3',
-    '세부 정보 3',
-    '준비 3',
-    '영양 정보 3',
-    'www.video_ko.com'
-);
-
-CALL insert_recipe_translation_by_iso_code(
-    4, 'en',
-    'Title 4',
-    'Details 4',
-    'Preparation 4',
-    'Nutritional information 4',
-    'www.video.com'
-);
-
-CALL insert_recipe_translation_by_iso_code(
-    4, 'es',
-    'Título 4',
-    'Detalles 4',
-    'Preparación 4',
-    'Información nutricional 4',
-    'www.video_es.com'
-);
-
-CALL insert_recipe_translation_by_iso_code(
-    4, 'fr',
-    'Titre 4',
-    'Détails 4',
-    'Préparation 4',
-    'Informations nutritionnelles 4',
-    'www.video_fr.com'
-);
-
-CALL insert_recipe_translation_by_iso_code(
-    4, 'ko',
-    '제목 4',
-    '세부 정보 4',
-    '준비 4',
-    '영양 정보 4',
-    'www.video_ko.com'
-);
-
-CALL insert_recipe_translation_by_iso_code(
-    5, 'en',
-    'Title 5',
-    'Details 5',
-    'Preparation 5',
-    'Nutritional information 5',
-    'www.video.com'
-);
-
-CALL insert_recipe_translation_by_iso_code(
-    5, 'es',
-    'Título 5',
-    'Detalles 5',
-    'Preparación 5',
-    'Información nutricional 5',
-    'www.video_es.com'
-);
-
-CALL insert_recipe_translation_by_iso_code(
-    5, 'fr',
-    'Titre 5',
-    'Détails 5',
-    'Préparation 5',
-    'Informations nutritionnelles 5',
-    'www.video_fr.com'
-);
-
-CALL insert_recipe_translation_by_iso_code(
-    5, 'ko',
-    '제목 5',
-    '세부 정보 5',
-    '준비 5',
-    '영양 정보 5',
-    'www.video_ko.com'
-);
+CALL insert_recipe_translation_by_iso_code(1, 'en', 'Title 1', 'Details 1', 'Preparation 1', 'Nutritional information 1', 'www.video.com');
+CALL insert_recipe_translation_by_iso_code(1, 'es', 'Título 1', 'Detalles 1', 'Preparación 1', 'Información nutricional 1', 'www.video_es.com');
+CALL insert_recipe_translation_by_iso_code(1, 'fr', 'Titre 1', 'Détails 1', 'Préparation 1', 'Informations nutritionnelles 1', 'www.video_fr.com');
+CALL insert_recipe_translation_by_iso_code(1, 'ko', '제목 1', '세부 정보 1', '준비 1', '영양 정보 1', 'www.video_ko.com');
+CALL insert_recipe_translation_by_iso_code(2, 'en', 'Title 2', 'Details 2', 'Preparation 2', 'Nutritional information 2', 'www.video.com');
+CALL insert_recipe_translation_by_iso_code(2, 'es', 'Título 2', 'Detalles 2', 'Preparación 2', 'Información nutricional 2', 'www.video_es.com');
+CALL insert_recipe_translation_by_iso_code(2, 'fr', 'Titre 2', 'Détails 2', 'Préparation 2', 'Informations nutritionnelles 2', 'www.video_fr.com');
+CALL insert_recipe_translation_by_iso_code(2, 'ko', '제목 2', '세부 정보 2', '준비 2', '영양 정보 2', 'www.video_ko.com');
+CALL insert_recipe_translation_by_iso_code(3, 'en', 'Title 3', 'Details 3', 'Preparation 3', 'Nutritional information 3', 'www.video.com');
+CALL insert_recipe_translation_by_iso_code(3, 'es', 'Título 3', 'Detalles 3', 'Preparación 3', 'Información nutricional 3', 'www.video_es.com');
+CALL insert_recipe_translation_by_iso_code(3, 'fr', 'Titre 3', 'Détails 3', 'Préparation 3', 'Informations nutritionnelles 3', 'www.video_fr.com');
+CALL insert_recipe_translation_by_iso_code(3, 'ko', '제목 3', '세부 정보 3', '준비 3', '영양 정보 3', 'www.video_ko.com');
+CALL insert_recipe_translation_by_iso_code(4, 'en', 'Title 4', 'Details 4', 'Preparation 4', 'Nutritional information 4', 'www.video.com');
+CALL insert_recipe_translation_by_iso_code(4, 'es', 'Título 4', 'Detalles 4', 'Preparación 4', 'Información nutricional 4', 'www.video_es.com');
+CALL insert_recipe_translation_by_iso_code(4, 'fr', 'Titre 4', 'Détails 4', 'Préparation 4', 'Informations nutritionnelles 4', 'www.video_fr.com');
+CALL insert_recipe_translation_by_iso_code(4, 'ko', '제목 4', '세부 정보 4', '준비 4', '영양 정보 4', 'www.video_ko.com');
+CALL insert_recipe_translation_by_iso_code(5, 'en', 'Title 5', 'Details 5', 'Preparation 5', 'Nutritional information 5', 'www.video.com');
+CALL insert_recipe_translation_by_iso_code(5, 'es', 'Título 5', 'Detalles 5', 'Preparación 5', 'Información nutricional 5', 'www.video_es.com');
+CALL insert_recipe_translation_by_iso_code(5, 'fr', 'Titre 5', 'Détails 5', 'Préparation 5', 'Informations nutritionnelles 5', 'www.video_fr.com');
+CALL insert_recipe_translation_by_iso_code(5, 'ko', '제목 5', '세부 정보 5', '준비 5', '영양 정보 5', 'www.video_ko.com');
