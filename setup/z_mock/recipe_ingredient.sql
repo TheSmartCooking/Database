c7b5c3b868fab7e39d3476c3305dabbce580fe80
Vianpyro <vianney@veremme.org>
2024-12-28 23:15:16 -0500

    Refactor mock data insertion for recipe ingredients to improve readability by consolidating JSON strings

--- setup/z_mock/recipe_ingredient.sql
+++ setup/z_mock/recipe_ingredient.sql
@@ -5,12 +5,8 @@ USE smartcooking;
 CALL insert_recipe_ingredient(1, 1, 2, 'g');
 CALL insert_recipe_ingredient_by_name(1, 'Sugar', 1, 'tsp');
 CALL insert_recipe_ingredients(
-    2,
-    '[{"ingredient_id": 1, "quantity": 2, "unit": "g"},' ||
-    ' {"ingredient_id": 2, "quantity": 1, "unit": "tsp"}]'
+    2, '[{"ingredient_id": 1, "quantity": 2, "unit": "g"}, {"ingredient_id": 2, "quantity": 1, "unit": "tsp"}]'
 );
 CALL insert_recipe_ingredients_by_name(
-    3,
-    '[{"ingredient_name": "Salt", "quantity": 2, "unit": "g"},' ||
-    ' {"ingredient_name": "Pepper", "quantity": 1, "unit": "tsp"}]'
+    3, '[{"ingredient_name": "Salt", "quantity": 2, "unit": "g"}, {"ingredient_name": "Pepper", "quantity": 1, "unit": "tsp"}]'
 );
