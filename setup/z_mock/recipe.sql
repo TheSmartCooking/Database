d8d53af3e4f586824cd9c874c2eff0f3d290bc3f
Vianney Veremme <vianney@veremme.org>
2025-04-14 15:23:12 -0400

    Update `insert_recipe` procedure calls to match new parameters and include status name

--- setup/z_mock/recipe.sql
+++ setup/z_mock/recipe.sql
@@ -2,8 +2,8 @@
 USE smartcooking;
 
 -- Fill the database with mock data
-CALL insert_recipe(1, 1, 30, 3, 'www.source.com', 1);
-CALL insert_recipe(2, 2, 45, 2, 'www.source.com', 2);
-CALL insert_recipe(3, 3, 60, 1, 'www.source.com', 1);
-CALL insert_recipe(4, 4, 75, 3, 'www.source.com', 3);
-CALL insert_recipe(4, 5, 90, 2, 'www.source.com', 1);
+CALL insert_recipe(1, 1, 30, 5, 3, 1, 1, 'www.source.com', 'draft');
+CALL insert_recipe(2, 2, 45, 5, 2, 1, 2, 'www.source.com', 'draft');
+CALL insert_recipe(3, 3, 60, 10, 1, 2, 1, 'www.source.com', 'draft');
+CALL insert_recipe(4, 4, 75, 15, 3, 3, 3, 'www.source.com', 'draft');
+CALL insert_recipe(4, 5, 90, 100, 2, 3, 2, 'www.source.com', 'draft');
